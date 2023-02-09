9ba035166ae49e29a1580bfb30053208db77ce82
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-09 01:06:02 -0500

    SA account name changed

--- create-cluster.groovy
+++ create-cluster.groovy
@@ -8,7 +8,7 @@ pipeline {
       steps {
         withCredentials([file(credentialsId: 'kubernetes-sa', variable: 'kubernetes-sa')]) {
           sh '''
-            gcloud auth activate-service-account --key-file="/secure/kubernetes-sa.json"
+            gcloud auth activate-service-account --key-file="/secure/kubernetes-account.json"
             gcloud container clusters create prod-cluster --machine-type n1-standard-2 --num-nodes 1 --zone us-central1-a --scopes "https://www.googleapis.com/auth/source.read_write,cloud-platform" --cluster-version latest
           '''
         }
