fb1dc381efc1bd4c5010be4cd73a0f484f93b55e
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-09 01:21:05 -0500

    sa var changed

--- create-cluster.groovy
+++ create-cluster.groovy
@@ -6,7 +6,7 @@ pipeline {
   stages {
     stage('test') {
       steps {
-        withCredentials([file(credentialsId: 'kubernetes-sa', variable: 'kubernetes-sa')]) {
+        withCredentials([file(credentialsId: 'kubernetes-account', variable: 'kubernetes-account')]) {
           sh '''
             gcloud auth activate-service-account --key-file="/secure/kubernetes-account.json"
             gcloud container clusters create prod-cluster --machine-type n1-standard-2 --num-nodes 1 --zone us-central1-a --scopes "https://www.googleapis.com/auth/source.read_write,cloud-platform" --cluster-version latest
