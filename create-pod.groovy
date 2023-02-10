4970b6d6fa22871d91ab8772123bc3bbb52b580b
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-10 01:11:22 -0500

    changed

--- create-pod.groovy
+++ create-pod.groovy
@@ -8,7 +8,7 @@ pipeline {
       steps {
         withCredentials([file(credentialsId: 'jenkins-kube-sa', variable: 'jenkins-kube-sa')]) {
           sh '''
-            gcloud auth activate-service-account --key-file="/secure/jenkins-kube-sa.json"
+            #gcloud auth activate-service-account --key-file="/secure/jenkins-kube-sa.json"
             kubectl create deployment highway-webserver --image=us.gcr.io/burner-noohauqu/highway-site:v1
             kubectl expose deployment highway-webserver --type LoadBalancer --port 80 --target-port 80
             Kubectl get deployments -o wide
