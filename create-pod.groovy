81026c591f8bd2b56b15312ddf1429cc653c133e
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-10 01:06:48 -0500

    creation pod

---
+++ create-pod.groovy
@@ -0,0 +1,20 @@
+pipeline {
+  agent any
+  environment {
+    CLOUDSDK_CORE_PROJECT='burner-noohauqu'
+  }
+  stages {
+    stage('test') {
+      steps {
+        withCredentials([file(credentialsId: 'jenkins-kube-sa', variable: 'jenkins-kube-sa')]) {
+          sh '''
+            gcloud auth activate-service-account --key-file="/secure/jenkins-kube-sa.json"
+            kubectl create deployment highway-webserver --image=us.gcr.io/burner-noohauqu/highway-site:v1
+            kubectl expose deployment highway-webserver --type LoadBalancer --port 80 --target-port 80
+            Kubectl get deployments -o wide
+          '''
+        }
+      }
+    }
+  }
+}
