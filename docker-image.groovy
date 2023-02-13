0dfd2028af95f4b3ab7a7e4e2375a985c1814bdd
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-13 02:37:43 -0500

    create container

---
+++ docker-image.groovy
@@ -0,0 +1,17 @@
+pipeline {
+  agent any
+  environment {
+    CLOUDSDK_CORE_PROJECT='burner-noohauqu'
+  }
+  stages {
+    stage('test') {
+      steps {
+        withCredentials([file(credentialsId: 'jenkins-kube-sa', variable: 'jenkins-kube-sa')]) {
+          sh '''
+            docker system prune -a --volumes -f
+            docker run -d -p 8050:80 nginx:latest
+            for i in `docker ps -a | awk '{print $1}' `;do docker exec -itd $i /bin/bash && git clone https://github.com/noohauqu/covid19-site.git && rm -rf /usr/share/nginx/html/* && cd "covid site" && cp -r . /usr/share/nginx/html/; done 
+          '''
+        }
+      }
+    }
