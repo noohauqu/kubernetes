47a5b9ab372bb9a33ba07628d87ab9e5478d463e
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-13 03:06:16 -0500

    create image1

--- docker-image.groovy
+++ docker-image.groovy
@@ -9,6 +9,7 @@ pipeline {
         withCredentials([file(credentialsId: 'jenkins-kube-sa', variable: 'jenkins-kube-sa')]) {
           sh '''
             docker system prune -a --volumes -f
+            docker logn noohauqu/Docker@123
             docker run -d -p 8050:80 nginx:latest
             for i in `docker ps -a | awk '{print $1}' `;do docker exec -itd $i /bin/bash && git clone https://github.com/noohauqu/covid19-site.git && rm -rf /usr/share/nginx/html/* && cd "covid site" && cp -r . /usr/share/nginx/html/; done
             
