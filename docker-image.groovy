9795913b1103c7692767ea8232b1a5c84e5551a0
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-13 06:55:08 -0500

    image

--- docker-image.groovy
+++ docker-image.groovy
@@ -10,7 +10,7 @@ pipeline {
           sh '''
             docker system prune -a --volumes -f
             docker login  --username noohauqu --password Docker@123
-            docker run -d -p 8050:80 docker.io/library/nginx:latest
+            docker run -d -p 8050:80 3f8a00f137a0
             for i in `docker ps -a | awk '{print $1}' `;do docker exec -itd $i /bin/bash && git clone https://github.com/noohauqu/covid19-site.git && rm -rf /usr/share/nginx/html/* && cd "covid site" && cp -r . /usr/share/nginx/html/; done
             
           '''
