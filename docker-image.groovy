653df3c308f5ea267abf38b9512c7e47add4d5ae
Noorul Haque <noohauqu@publicisgroupe.net>
2023-02-13 07:57:37 -0500

    commit

--- docker-image.groovy
+++ docker-image.groovy
@@ -10,8 +10,8 @@ pipeline {
           sh '''
             docker system prune -a --volumes -f
             docker login  --username noohauqu --password Docker@123
-            docker run -d -p 8050:80 3f8a00f137a0
-            for i in `docker ps -a | awk '{print $1}' `;do docker exec -itd $i /bin/bash && git clone https://github.com/noohauqu/covid19-site.git && rm -rf /usr/share/nginx/html/* && cd "covid site" && cp -r . /usr/share/nginx/html/; done
+            docker run -d -p 8050:80 3a4ea134cf8e
+            for i in `docker ps -a | awk '{print $1}' `;do docker exec -it $i /bin/bash && git clone https://github.com/noohauqu/covid19-site.git && rm -rf /usr/local/apache2/htdocs/* && cd "covid site" && cp -r . /usr/local/apache2/htdocs/; done
             
           '''
         }
