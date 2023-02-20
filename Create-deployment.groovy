054cb7564735970a9aae7a864520583bddc6380d
noohauqu <test@abc.com>
2023-02-20 07:14:22 +0000

    deploy

---
+++ Create-deployment.groovy
@@ -0,0 +1,18 @@
+pipeline {
+  agent any
+  environment {
+    CLOUDSDK_CORE_PROJECT='burner-noohauqu'
+  }
+  stages {
+    stage('test') {
+      steps {
+        withCredentials([file(credentialsId: 'kubernetes-account', variable: 'kubernetes-account')]) {
+          sh '''
+            gcloud auth activate-service-account --key-file="/secure/kubernetes-account.json"
+            kubectl apply -f https://github.com/noohauqu/kubernetes/blob/main/deployment.yaml
+          '''
+        }
+      }
+    }
+  }
+}
