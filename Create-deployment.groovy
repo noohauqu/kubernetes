36d2d80df4ad3737e4e978e6f822ea480b7c1dbd
noohauqu <test@abc.com>
2023-02-20 07:24:35 +0000

    testcommit1

--- Create-deployment.groovy
+++ Create-deployment.groovy
@@ -9,6 +9,7 @@ pipeline {
         withCredentials([file(credentialsId: 'kubernetes-account', variable: 'kubernetes-account')]) {
           sh '''
             gcloud auth activate-service-account --key-file="/secure/kubernetes-account.json"
+            kubectl apply -f https://github.com/noohauqu/kubernetes/blob/main/create-ns.yaml
             kubectl apply -f https://github.com/noohauqu/kubernetes/blob/main/deployment.yaml
           '''
         }
